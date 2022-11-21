0c3ee58122e7b039dadba51cc62dde962bfe9710
SivaniPulagam <109748543+SivaniPulagam@users.noreply.github.com>
2022-11-21 08:29:51 -0800

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,4 @@
+a=int(input("enter value for a:"))
+b=int(input("enter value for b:"))
+c=a+b
+print("addition of two numbers is:",c)
---
+++ division.py
@@ -0,0 +1,3 @@
+a=int(input("enter value for a:"))
+b=int(input("enter value for b:"))
+print("division of two numbers is:",a/b)
