fe2f16e341437f2f48fbeb4be5714534e3fe5087
SivaniPulagam <109748543+SivaniPulagam@users.noreply.github.com>
2022-11-25 08:10:49 -0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,7 @@
+list=[1,2,3,4,5,6,7,8,9]
+print("Enter any number:")
+n=int(input())
+if n in list:
+    print("Given number is in list")
+else:
+    print("Given number is not in the list")
---
+++ min.py
@@ -0,0 +1,6 @@
+list=[12,23,34,56,78]
+min=list[0]
+for i in list:
+    if i<min:
+        min=i
+print("Minimum value from the list is:",min)
